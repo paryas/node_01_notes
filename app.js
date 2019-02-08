33019959fc7dc1a0c734813d7e656d2240560fb3
paryas <pouyanparyas@outlook.com>
2019-02-08 07:50:47 -0800

    4th

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ console.log('starting app...');
 const fs = require('fs');
 ///os = operating system module
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+console.log('Result:', notes.add(9, -2));
+
+//var user = os.userInfo();
 //console.log(user);
 
+// var res = notes.addNote();
+// console.log(res);
+
 ///appendFile to create the txt file and append into it
-//fs.appendFileSync('greeting.txt', 'Hello world\r\n');
-fs.appendFileSync('greeting.txt', `Hello ${user.username}\r\n`);
-//fs.appendFileSync('D:\\greeting.txt', 'Hello world');
+// fs.appendFileSync('greeting.txt', 'Hello world\r\n');
+// fs.appendFileSync(
+//   'greeting.txt',
+//   `\r\nHello ${user.username}! You are ${notes.age}.`
+// );
+// fs.appendFileSync('D:\\greeting.txt', 'Hello world');
 
 console.log('Ending app...');
